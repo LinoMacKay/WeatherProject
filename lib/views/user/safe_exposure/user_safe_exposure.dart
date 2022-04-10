cf038eeceee0684d5828ffd2529101b81ae29d22
Guillermo Gavilano <ggavilano@itana.pe>
2022-04-10 15:58:31 -0500

    Modificando pantalla de safe exposure user

--- lib/views/user/safe_exposure/user_safe_exposure.dart
+++ lib/views/user/safe_exposure/user_safe_exposure.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:my_project/core/ui/decorated_text_component.dart';
-import 'package:my_project/core/ui/labeled_text_form_component.dart';
 import 'package:my_project/core/ui/user_header_component.dart';
+import 'package:my_project/data/viewmodels/create_children_quote.dart';
 import 'package:my_project/helper/ui/ui_library.dart';
 
-class UserSafeExposure extends StatelessWidget {
+class UserSafeExposure extends StatefulWidget {
   const UserSafeExposure({Key? key}) : super(key: key);
 
+  @override
+  State<UserSafeExposure> createState() => _UserSafeExposureState();
+}
+
+class _UserSafeExposureState extends State<UserSafeExposure> {
+  final TextEditingController ageController = TextEditingController();
+  final FocusNode ageFocusNode = FocusNode();
+  final TextStyle style = const TextStyle();
+
+  List<QuoteOption> quoteOptions = [
+    QuoteOption(id: '1', description: 'I'),
+    QuoteOption(id: '1', description: 'II'),
+    QuoteOption(id: '1', description: 'III'),
+    QuoteOption(id: '1', description: 'IV'),
+    QuoteOption(id: '1', description: 'V'),
+    QuoteOption(id: '1', description: 'VI'),
+  ];
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
@@ -16,18 +32,75 @@ class UserSafeExposure extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Padding(
+              const SizedBox(height: 20),
+              const Padding(
                 padding: EdgeInsets.only(left: 30),
                 child: UserHeaderComponent(showProfile: false),
               ),
-              SizedBox(height: 20),
-              LabeledTextFormComponent(label: 'Age', hint: '7'),
-              SizedBox(height: 8),
-              LabeledTextFormComponent(label: 'Fototype', hint: 'I'),
-              SizedBox(height: 90),
-              DecoratedTextComponent(text: 'CALCULATE EXPOSURE'),
-              SizedBox(height: 160),
-              Text('Exposición máxima de 20 minutos', textAlign: TextAlign.center),
+              const SizedBox(height: 40),
+              const Text("Safe Exposure"),
+              AppTextForm(
+                isLeftLabel: true,
+                label: 'Age',
+                width: 300,
+                controller: ageController,
+                focusNode: ageFocusNode,
+                style: style,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text("Tipo"),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 200,
+                    decoration: const ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1.0, style: BorderStyle.solid),
+                      ),
+                    ),
+                    alignment: Alignment.topCenter,
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<QuoteOption>(
+                        items: quoteOptions
+                            .map((QuoteOption option) =>
+                                DropdownMenuItem<QuoteOption>(
+                                  child: Center(
+                                    child: Text(
+                                      option.description,
+                                    ),
+                                  ),
+                                  value: option,
+                                ))
+                            .toList(),
+                        iconSize: 20,
+                        hint: Container(
+                          alignment: Alignment.centerLeft,
+                          child: const Text(
+                            'Seleccione su respuesta',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        value: null,
+                        onChanged: (option) {},
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+              AppButton(
+                onPressed: () {},
+                text: 'CALCULATE EXPOSURE',
+                width: 200,
+              ),
+              const SizedBox(height: 160),
+              const Text('Exposición máxima de 20 minutos',
+                  textAlign: TextAlign.center),
             ],
           ),
         ),
