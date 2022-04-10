faff59e4e5e3376c1d69ed07143710da6fcdd9a4
LinoMacKay <u201819681@upc.edu.pe>
2022-04-09 22:50:00 -0500

    Update children_create_page_four.dart

--- lib/views/children/create/pages/page_four/children_create_page_four.dart
+++ lib/views/children/create/pages/page_four/children_create_page_four.dart
@@ -49,93 +49,95 @@ class _ChildrenCreatePageFourState extends State<ChildrenCreatePageFour> {
     var index = 0;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const SizedBox(height: 10),
-          const Text('Registro de hijo (paso 4 de 4) - Confirmación'),
-          const SizedBox(height: 20),
-          const Text('Se procederá a registrar la información:'),
-          const SizedBox(height: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text('Datos Generales'),
-              Container(
-                width: double.infinity,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 10),
+            const Text('Registro de hijo (paso 4 de 4) - Confirmación'),
+            const SizedBox(height: 20),
+            const Text('Se procederá a registrar la información:'),
+            const SizedBox(height: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text('Datos Generales'),
+                Container(
+                  width: double.infinity,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('Nombre: ${widget.form.nameController.text}'),
+                      Text('Fecha de Nacimiento: ${widget.form.dateFormatted}'),
+                      Text(
+                          'Edad: ${widget.form.age} ${widget.form.age < 2 ? "año" : "años"}'),
+                    ],
+                  ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Nombre: ${widget.form.nameController.text}'),
-                    Text('Fecha de Nacimiento: ${widget.form.dateFormatted}'),
-                    Text(
-                        'Edad: ${widget.form.age} ${widget.form.age < 2 ? "año" : "años"}'),
-                  ],
+              ],
+            ),
+            const SizedBox(height: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text('Cuestionario'),
+                Container(
+                  width: double.infinity,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: widget.selectedOptions
+                        .map<Widget>((e) => Text(
+                            'Pregunta ${index++}: ${e?.description ?? ""}'))
+                        .toList(),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text('Cuestionario'),
-              Container(
-                width: double.infinity,
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: widget.selectedOptions
-                      .map<Widget>((e) =>
-                          Text('Pregunta ${index++}: ${e?.description ?? ""}'))
-                      .toList(),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          const Align(
-            alignment: Alignment.center,
-            child: Text('Escala Fitzpatrick'),
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text('Fototipo'),
-              FototipoOptionComponent(model: widget.fototipoOptionViewmodel),
-            ],
-          ),
-          Expanded(child: SizedBox()),
-          const Align(
-            alignment: Alignment.center,
-            child: Text('¿Es correcta la información?'),
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              AppButton(onPressed: widget.onBack, text: 'Regresar', width: 100),
-              AppButton(
-                  onPressed: () {
-                    Utils.homeNavigator.currentState!
-                        .pushReplacementNamed(routeHome);
-                  },
-                  text: 'Continuar',
-                  width: 100),
-            ],
-          ),
-        ],
+              ],
+            ),
+            const SizedBox(height: 10),
+            const Align(
+              alignment: Alignment.center,
+              child: Text('Escala Fitzpatrick'),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text('Fototipo'),
+                FototipoOptionComponent(model: widget.fototipoOptionViewmodel),
+              ],
+            ),
+            const Align(
+              alignment: Alignment.center,
+              child: Text('¿Es correcta la información?'),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                AppButton(
+                    onPressed: widget.onBack, text: 'Regresar', width: 100),
+                AppButton(
+                    onPressed: () {
+                      Utils.homeNavigator.currentState!
+                          .pushReplacementNamed(routeHome);
+                    },
+                    text: 'Continuar',
+                    width: 100),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
